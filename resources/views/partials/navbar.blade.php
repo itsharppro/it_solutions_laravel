f39ee627a33a88597eabd194e00dc4a28d3205f3
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-08 03:08:24 +0200

    (#1) layout: update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,25 +9,38 @@
             </button>
             <div class="collapse navbar-collapse bg-transparent" id="navbarCollapse">
                 <div class="navbar-nav ms-auto mx-xl-auto p-0">
-                    <a href="{{ url('/') }}" class="nav-item nav-link">Home</a>
-                    <a href="{{ url('/about') }}" class="nav-item nav-link">About</a>
-                    <a href="{{ url('/services') }}" class="nav-item nav-link">Services</a>
-                    <a href="{{ url('/projects') }}" class="nav-item nav-link active">Projects</a>
+                    <!-- Home -->
+                    <a href="{{ url('/') }}" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}">Home</a>
+
+                    <!-- About -->
+                    <a href="{{ url('/about') }}" class="nav-item nav-link {{ Request::is('about') ? 'active' : '' }}">About</a>
+
+                    <!-- Services -->
+                    <a href="{{ url('/services') }}" class="nav-item nav-link {{ Request::is('services') ? 'active' : '' }}">Services</a>
+
+                    <!-- Projects -->
+                    <a href="{{ url('/projects') }}" class="nav-item nav-link {{ Request::is('projects') ? 'active' : '' }}">Projects</a>
+
+                    <!-- Pages Dropdown -->
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle {{ Request::is('blog') || Request::is('team') || Request::is('testimonials') || Request::is('404') ? 'active' : '' }}" data-bs-toggle="dropdown">Pages</a>
                         <div class="dropdown-menu rounded">
-                            <a href="{{ url('/blog') }}" class="dropdown-item">Our Blog</a>
-                            <a href="{{ url('/team') }}" class="dropdown-item">Our Team</a>
-                            <a href="{{ url('/testimonials') }}" class="dropdown-item">Testimonial</a>
-                            <a href="{{ url('/404') }}" class="dropdown-item">404 Page</a>
+                            <a href="{{ url('/blog') }}" class="dropdown-item {{ Request::is('blog') ? 'active' : '' }}">Our Blog</a>
+                            <a href="{{ url('/team') }}" class="dropdown-item {{ Request::is('team') ? 'active' : '' }}">Our Team</a>
+                            <a href="{{ url('/testimonials') }}" class="dropdown-item {{ Request::is('testimonials') ? 'active' : '' }}">Testimonial</a>
+                            <a href="{{ url('/404') }}" class="dropdown-item {{ Request::is('404') ? 'active' : '' }}">404 Page</a>
                         </div>
                     </div>
-                    <a href="{{ url('/contact') }}" class="nav-item nav-link">Contact</a>
+
+                    <!-- Contact -->
+                    <a href="{{ url('/contact') }}" class="nav-item nav-link {{ Request::is('contact') ? 'active' : '' }}">Contact</a>
                 </div>
             </div>
+
+            <!-- Additional Info -->
             <div class="d-none d-xl-flex flex-shrink-0">
                 <div id="phone-tada" class="d-flex align-items-center justify-content-center me-4">
-                    <a href="" class="position-relative animated tada infinite">
+                    <a href="#" class="position-relative animated tada infinite">
                         <i class="fa fa-phone-alt text-white fa-2x"></i>
                         <div class="position-absolute" style="top: -7px; left: 20px;">
                             <span><i class="fa fa-comment-dots text-secondary"></i></span>
